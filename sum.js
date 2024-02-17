7d5dd2fd1212149e286516c98f0456dd1fe9d95d
consciousnessdev <consciousness.dev@gmail.com>
2024-02-17 18:18:00 +0700

    async testing: callback fetch data test, resolve test, reject test, async-await test

--- sum.js
+++ sum.js
@@ -8,7 +8,23 @@ function myFunction(input) {
   }
 }
 
+function fetchData(callback) {
+  setTimeout(() => {
+    callback("peanut butter");
+  }, 1000);
+}
+
+function fetchPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("peanut butter");
+    }, 1000);
+  });
+}
+
 module.exports = {
   sum,
   myFunction,
+  fetchData,
+  fetchPromise,
 };
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const { sum, myFunction } = require("./sum");
+const { sum, myFunction, fetchData, fetchPromise } = require("./sum");
 
 // test("[Description] adds 1 + 2 to equal 3", () => {
 //   // expected is expecting output from what to test
@@ -31,16 +31,45 @@ const { sum, myFunction } = require("./sum");
 // });
 
 // tThrow
-test("throws on invalid input", () => {
-  expect(() => {
-    // myFunction("wew");
-    // above invalid, will throw error
-    myFunction(3);
-    // above valid, will not throw error, because myFunction return
-    // if not number then throw error
-  })
-    // settled expected output are throw error
-    .toThrow();
-  // when its throwed error so its passed, because expect to throwed error
-  // if its not throwed error it will be not passed
-});
+// test("throws on invalid input", () => {
+//   expect(() => {
+//     // myFunction("wew");
+//     // above invalid, will throw error
+//     myFunction(3);
+//     // above valid, will not throw error, because myFunction return
+//     // if not number then throw error
+//   })
+//     // settled expected output are throw error
+//     .toThrow();
+//   // when its throwed error so its passed, because expect to throwed error
+//   // if its not throwed error it will be not passed
+// });
+
+// callback on fetch data test
+// test("the data is peanut butter", (done) => {
+//   function callback(data) {
+//     try {
+//       expect(data).toBe("peanut butter");
+//       done();
+//     } catch (error) {
+//       done(error);
+//     }
+//   }
+
+//   fetchData(callback);
+// });
+
+// // test async resolve
+// test("the data is peanut butter", () => {
+//   return expect(fetchPromise()).resolves.toBe("peanut butter");
+// });
+
+// // test async reject
+// test("the fetch fail with an error", () => {
+//   return expect(fetchPromise()).reject.toThrow("error");
+// });
+
+// test("the data is peanut butter promise", async () => {
+//   const data = await fetchPromise();
+//   expect(data).toBe("peanut butter");
+// });
